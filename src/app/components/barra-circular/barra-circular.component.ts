84ae8fb092ae460ff2c047b66fd396a9d74da936
lpolobeltran <luispolofb@gmail.com>
2024-11-30 00:32:19 -0500

    hasta aqui la barra circular tiene vida

--- src/app/components/barra-circular/barra-circular.component.ts
+++ src/app/components/barra-circular/barra-circular.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-barra-circular',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './barra-circular.component.html',
   styleUrl: './barra-circular.component.scss'
 })
-export class BarraCircularComponent {
+export class BarraCircularComponent implements OnInit {
+  degree: number = 0;  // Valor inicial
+  targetDegree: number = 100;  // Valor objetivo
+  color: string = '#fee800';  // Color del gradiente
+  circleStyle: { [key: string]: string } = {};  // Para el estilo dinámico
 
+  ngOnInit() {
+    this.animateCircle();
+  }
+
+  animateCircle() {
+    let degree = 0;
+    const interval = setInterval(() => {
+      degree += 1;
+      if (degree > this.targetDegree) {
+        clearInterval(interval);
+      }
+      this.degree = degree;
+      this.circleStyle['background'] = `conic-gradient(${this.color} ${degree}%, #222 0%)`;
+    }, 50);
+  }
 }

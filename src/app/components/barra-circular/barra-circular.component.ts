af86326e179bd1d5f995c8ae1f70d66a87df1e7c
lpolobeltran <luispolofb@gmail.com>
2024-11-30 00:53:58 -0500

    hasta aqui la barra circular tiene vida y no excede en 1

--- src/app/components/barra-circular/barra-circular.component.ts
+++ src/app/components/barra-circular/barra-circular.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,13 +6,14 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './barra-circular.component.html',
-  styleUrl: './barra-circular.component.scss'
+  styleUrls: ['./barra-circular.component.scss']
 })
 export class BarraCircularComponent implements OnInit {
   degree: number = 0;  // Valor inicial
-  targetDegree: number = 100;  // Valor objetivo
+  targetDegree: number = 15.5;  // Valor objetivo
   color: string = '#fee800';  // Color del gradiente
   circleStyle: { [key: string]: string } = {};  // Para el estilo dinámico
+  numberStyle: { [key: string]: string } = {};  // Para el estilo dinámico del número
 
   ngOnInit() {
     this.animateCircle();
@@ -21,12 +22,18 @@ export class BarraCircularComponent implements OnInit {
   animateCircle() {
     let degree = 0;
     const interval = setInterval(() => {
-      degree += 1;
-      if (degree > this.targetDegree) {
+      if (degree >= this.targetDegree) {  // Verificamos si ya alcanzó el objetivo
+        degree = this.targetDegree;  // Aseguramos que no se pase de 100
         clearInterval(interval);
+      } else {
+        degree += 1;
       }
-      this.degree = degree;
+
+      this.degree = degree;  // Actualiza el valor de la animación
       this.circleStyle['background'] = `conic-gradient(${this.color} ${degree}%, #222 0%)`;
+
+      // Actualiza el color del número dinámicamente
+      this.numberStyle['color'] = this.color;  // Asigna el mismo color al número
     }, 50);
   }
 }

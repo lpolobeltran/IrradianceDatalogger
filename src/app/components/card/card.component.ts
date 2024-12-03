410cfa16c67c99babdcd61f97bb2c0381b23a035
lpolobeltran <luispolofb@gmail.com>
2024-12-02 23:59:36 -0500

    hasta aqui hay una comunicacion inicial entre sheet y angular

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  @Input() temperature: number = 0; // Valor predeterminado en caso de no recibir entrada
+
+  //  función para calcular el desplazamiento de progreso en función de la temperatura:
+  calculateDashOffset(temperature: number): string {
+    const maxTemperature = 100; // Cambia este valor según el rango máximo esperado
+    const percentage = Math.min((temperature / maxTemperature) * 100, 100); // Asegura que no exceda el 100%
+
+    const circumference = 2 * Math.PI * 40; // Fórmula: 2 * π * radio
+    const dashOffset = circumference * (1 - percentage / 100);
+    return `${dashOffset}px`;
+  }
 
 }
--- src/app/pages/ambiental/ambiental.component.ts
+++ src/app/pages/ambiental/ambiental.component.ts
@@ -1,26 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { GoogleSheetsService } from '../../services/google-sheets.service';
 import { Component, OnInit } from '@angular/core'; // Se importa OnInit para poder usarlo
-import { MenuComponent } from '../../components/menu/menu.component';
 import { CardComponent } from "../../components/card/card.component";
 
 
 @Component({
   selector: 'app-ambiental',
   standalone: true,
-  imports: [CommonModule, MenuComponent, CardComponent], // Necesario para
+  imports: [CommonModule, CardComponent], // Necesario para
   templateUrl: './ambiental.component.html',
   styleUrl: './ambiental.component.scss'
 })
 export class AmbientalComponent implements OnInit {
-  public data: any[] = []; // Variable para almacenar los datos que vienen del servicio
+  public temperature: number = 0; // Último valor de la temperatura
 
   constructor(private googleSheetsService: GoogleSheetsService) {}
 
   ngOnInit(): void {
     this.googleSheetsService.getDataWithPolling(5000).subscribe({
       next: (data) => {
-        this.data = data; // Asigna los datos recibidos a "data"
+        const latestRow = data[data.length - 1]; // Asume que la última fila tiene el dato más reciente
+        this.temperature = latestRow[1]; // Cambia [1] por el índice de la columna que contiene la temperatura
       },
       error: (err) => console.error('Error al obtener datos:', err),
     });

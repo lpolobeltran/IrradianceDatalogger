37d24c5b111ac7f420e16d696005ae34ad909a29
lpolobeltran <luispolofb@gmail.com>
2024-12-05 22:21:23 -0500

    Se empieza a trabajar con los botones del filtro

---
+++ src/app/pages/comparacion-irradiancia/comparacion-irradiancia.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { GraficComponent } from '../../components/grafic/grafic.component';
+
+@Component({
+  selector: 'app-comparacion-irradiancia',
+  standalone: true,
+  imports: [GraficComponent],
+  templateUrl: './comparacion-irradiancia.component.html',
+  styleUrl: './comparacion-irradiancia.component.scss'
+})
+export class ComparacionIrradianciaComponent {
+
+}

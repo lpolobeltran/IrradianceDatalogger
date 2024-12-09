daee1cb5cb6cd9efa8585e99edc8b353caa6da8f
lpolobeltran <luispolofb@gmail.com>
2024-12-08 20:56:13 -0500

    Ajuste: se vincula cada card con su sensor correspondiente

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -10,20 +10,72 @@ import { Component, Input } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  // @Input() temperature: number = 0; // Valor predeterminado en caso de no recibir entrada
-  @Input() data!: { titulo: string; stroke: string,  temperatura?: number, humedad?:number, svg:string, myColor:string }; // Escucha un objeto con múltiples valores
-  hhs='black'
+
+  @Input() data!: {
+    titulo: string; stroke: string,
+    temperatura?: number,
+    humedad?:number,
+    temperaturaIn?:number,
+    corriente?:number,
+    voltaje?:number,
+    irradiacionPatron?:number,
+    irradiacionProto?:number,
+    irradiacionPanel?:number,
+    svg:string,
+    myColor:string
+  }; // Escucha un objeto con múltiples valores
+
   /* En TypeScript, todas las propiedades deben ser inicializadas antes de usarse. Si declaras una propiedad sin inicializarla,
   TypeScript asume que podría ser undefined, lo cual puede causar errores en tiempo de compilación. Angular asignará el valor más
   adelante, pero TypeScript no lo sabe y da un error. Si estás seguro de que Angular u otro mecanismo inicializará la propiedad
   correctamente, puedes usar ! para evitar el error de compilación */
 
-  //  función para calcular el desplazamiento de progreso en función de la temperatura:
-  calculateDashOffset(temperatura: number | undefined): string {
+  //  Función para calcular el desplazamiento de progreso en función de la temperatura:
+  calculateDashOffsetTemperatura(value: number | undefined): string {
 
-    const validTemperature = temperatura ?? 0;
-    const maxTemperature = 100; // Cambia este valor según el rango máximo esperado
-    const percentage = Math.min((validTemperature / maxTemperature) * 100, 100); // Asegura que no exceda el 100%
+    const validValue = value ?? 0;
+    const maxValue = 100; // Cambia este valor según el rango máximo esperado
+    const percentage = Math.min((validValue / maxValue) * 100, 100); // Asegura que no exceda el 100%
+
+    const circumference = 2 * Math.PI * 40; // Fórmula: 2 * π * radio
+    const dashOffset = circumference * (1 - percentage / 100);
+    return `${dashOffset}px`;
+
+  }
+
+
+  //  Función para calcular el desplazamiento de progreso en función de la corriente:
+  calculateDashOffsetCorriente(value: number | undefined): string {
+
+    const validValue = value ?? 0;
+    const maxValue = 30; // Cambia este valor según el rango máximo esperado
+    const percentage = Math.min((validValue / maxValue) * 100, 100); // Asegura que no exceda el 100%
+
+    const circumference = 2 * Math.PI * 40; // Fórmula: 2 * π * radio
+    const dashOffset = circumference * (1 - percentage / 100);
+    return `${dashOffset}px`;
+
+  }
+
+  //  Función para calcular el desplazamiento de progreso en función de la corriente:
+  calculateDashOffsetVoltaje(value: number | undefined): string {
+
+    const validValue = value ?? 0;
+    const maxValue = 25; // Cambia este valor según el rango máximo esperado
+    const percentage = Math.min((validValue / maxValue) * 100, 100); // Asegura que no exceda el 100%
+
+    const circumference = 2 * Math.PI * 40; // Fórmula: 2 * π * radio
+    const dashOffset = circumference * (1 - percentage / 100);
+    return `${dashOffset}px`;
+
+  }
+
+  //  Función para calcular el desplazamiento de progreso en función de la corriente:
+  calculateDashOffsetPatron(value: number | undefined): string {
+
+    const validValue = value ?? 0;
+    const maxValue = 1500; // Cambia este valor según el rango máximo esperado
+    const percentage = Math.min((validValue / maxValue) * 100, 100); // Asegura que no exceda el 100%
 
     const circumference = 2 * Math.PI * 40; // Fórmula: 2 * π * radio
     const dashOffset = circumference * (1 - percentage / 100);
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,6 +14,12 @@ import { CardComponent } from "../../components/card/card.component";
 export class HomeComponent implements OnInit {
   public temperatura: number = 0; // Último valor de la temperatura
   public humedad: number = 0; // Último valor de la temperatura
+  public temperaturaIn: number = 0; // Último valor de la temperatura
+  public corriente: number = 0; // Último valor de la temperatura
+  public voltaje: number = 0; // Último valor de la temperatura
+  public irradiacionPatron: number = 0; // Último valor de la temperatura
+  public irradiacionProto: number = 0; // Último valor de la temperatura
+  public irradiacionPanel: number = 0; // Último valor de la temperatura
 
   // Inyeccion del servicio "GoogleSheetsService"
   constructor(private googleSheetsService: GoogleSheetsService) { } // Servicio de Google Sheets
@@ -23,12 +29,20 @@ export class HomeComponent implements OnInit {
     // Nos suscribimos al observable que emite los datos actualizados
     this.googleSheetsService.latestTemperature.subscribe({
       next: (data) => {
+
         if (data && data.length) {
           const latestRow = data[data.length - 1];
           // console.log('Última fila de datos:', latestRow);
           this.temperatura = latestRow[1]; // Asumiendo que la temperatura está en la columna 1
           this.humedad = latestRow[2]; // Asumiendo que la temperatura está en la columna 1
+          this.temperaturaIn = latestRow[3]; // Asumiendo que la temperatura está en la columna 1
+          this.corriente = latestRow[4]; // Asumiendo que la temperatura está en la columna 1
+          this.voltaje = latestRow[5]; // Asumiendo que la temperatura está en la columna 1
+          this.irradiacionPatron = latestRow[6]; // Asumiendo que la temperatura está en la columna 1
+          this.irradiacionProto = latestRow[7]; // Asumiendo que la temperatura está en la columna 1
+          this.irradiacionPanel = latestRow[8]; // Asumiendo que la temperatura está en la columna 1
         }
+
       },
       error: (err) => console.error('Error al obtener datos:', err),
     });

9adfffe90ee27b62052666f4aaa9ead2e682d911
lpolobeltran <luispolofb@gmail.com>
2025-02-11 02:15:19 -0500

    Se termina version actualiza con distribucion de elementos apropiada, de añade animacion a los iconos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -9,6 +8,9 @@ import { ElectricoComponent } from './pages/electrico/electrico.component';
 import { AmbientalesComponent } from './pages/ambientales/ambientales.component';
 import { IntervalosComponent } from './pages/intervalos/intervalos.component';
 import { GraficComponent } from './components/grafic/grafic.component';
+import { CardComponent } from './components/card/card.component';
+import { GoogleSheetsService } from '../../src/app/services/google-sheets.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -24,16 +26,46 @@ import { GraficComponent } from './components/grafic/grafic.component';
     IntervalosComponent,
     GraficComponent,
     IntervalosComponent,
+    CardComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  activeComponent: string = 'home'; // Valor inicial
+  public temperatura: number = 0; // Último valor de la temperatura
+  public humedad: number = 0; // Último valor de la temperatura
+  public temperaturaIn: number = 0; // Último valor de la temperatura
+  public corriente: number = 0; // Último valor de la temperatura
+  public voltaje: number = 0; // Último valor de la temperatura
+  public irradiacionPatron: number = 0; // Último valor de la temperatura
+  public irradiacionProto: number = 0; // Último valor de la temperatura
+  public irradiacionPanel: number = 0; // Último valor de la temperatura
 
-  showComponent(component: string) {
-    this.activeComponent = component;
+  // Inyeccion del servicio "GoogleSheetsService"
+  constructor(private googleSheetsService: GoogleSheetsService) { } // Servicio de Google Sheets
+
+  ngOnInit(): void {
+    // Nos suscribimos al observable que emite los datos actualizados
+    this.googleSheetsService.latestTemperature.subscribe({
+      next: (data) => {
+
+        if (data && data.length) {
+          const latestRow = data[data.length - 1];
+          this.temperatura = latestRow[1]; // Asumiendo que la temperatura está en la columna 1
+          this.humedad = latestRow[2]; // Asumiendo que la temperatura está en la columna 1
+          this.temperaturaIn = latestRow[3]; // Asumiendo que la temperatura está en la columna 1
+          this.corriente = latestRow[4]; // Asumiendo que la temperatura está en la columna 1
+          this.voltaje = latestRow[5]; // Asumiendo que la temperatura está en la columna 1
+          this.irradiacionPatron = latestRow[6]; // Asumiendo que la temperatura está en la columna 1
+          this.irradiacionProto = latestRow[7]; // Asumiendo que la temperatura está en la columna 1
+          this.irradiacionPanel = latestRow[8]; // Asumiendo que la temperatura está en la columna 1
+        }
+
+      },
+      error: (err) => console.error('Error al obtener datos:', err),
+    });
   }
+
 }
--- src/app/components/grafic/grafic.component.ts
+++ src/app/components/grafic/grafic.component.ts
@@ -28,19 +28,19 @@ export class GraficComponent implements AfterViewInit {
     const ctx = (document.getElementById('grafic') as HTMLCanvasElement).getContext('2d');
     let labels = ["13:38", "13:37", "13:36", "13:36", "13:35",
       "13:35", "13:34", "13:34", "13:33", "13:33",
-      "13:32", "13:32", "13:31"];
+      "13:32", "13:32", "13:31", "13:31", "13:31", "13:31"];
 
 let datosRika = [627.273, 628.636, 612.273, 531.818, 424.091,
          432.273, 384.545, 436.364, 466.364, 617.727,
-         744.545, 743.182, 868.636];
+         744.545, 743.182, 868.636, 627.273, 628.636, 612.273, 531.818, 424.091];
 
 let datosProt = [667.342, 637.02, 625.848, 565.204, 491.792,
          466.257, 443.915, 458.278, 502.963, 619.465,
-         702.452, 720.007, 802.994];
+         702.452, 720.007, 802.994, 667.342, 637.02, 625.848, 565.204, 491.792];
 
 let datosPanel = [429.65, 429.33, 403.99, 336.94, 227.95,
           232.85, 187.51, 237.79, 267.59, 414.32,
-          545.91, 542.47, 673.61];
+          545.91, 542.47, 673.61, 429.65, 429.33, 403.99, 336.94, 227.95];
 
 
     // Verifica que el contexto exista antes de continuar
@@ -85,12 +85,19 @@ let datosPanel = [429.65, 429.33, 403.99, 336.94, 227.95,
           },
           responsive: true,
           plugins: {
+
             legend: {
               display: true,
+              labels: {
+                font: {
+                  size: 10
+                }
+              }
             },
             tooltip: {
               enabled: true,
             },
+
           },
           scales: {
             x: {
@@ -98,12 +105,22 @@ let datosPanel = [429.65, 429.33, 403.99, 336.94, 227.95,
               grid: {
                 display: false
               },
+              ticks: {
+                font: {
+                  size: 11 // 🔹 Tamaño de la letra en el eje X (ajusta según necesidad)
+                }
+              }
             },
             y: {
               beginAtZero: true,
               grid: {
                 display: false
               },
+              ticks: {
+                font: {
+                  size: 11 // 🔹 Tamaño de la letra en el eje X (ajusta según necesidad)
+                }
+              }
             },
           },
           elements: {
